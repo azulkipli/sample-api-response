ac85e9f5c62a9eae213a873471817479ae27677e
azulkipli <azulkipli@gmail.com>
2022-10-21 13:58:31 +0700

    res list voucher

--- voucherify/list-voucher.js
+++ voucherify/list-voucher.js
@@ -1,1 +1,104 @@
-https://github.com/azulkipli/sample-api-response.git
+const res = [
+    {
+        "id": "v_DlrYUlK4wv1Ewc4Z8acWkqc3LSm892KP",
+        "code": "DISC15MA",
+        "campaign": null,
+        "campaign_id": null,
+        "category": null,
+        "category_id": null,
+        "type": "DISCOUNT_VOUCHER",
+        "discount": {
+            "type": "PERCENT",
+            "amount_limit": 3000000,
+            "percent_off": 15,
+            "effect": "APPLY_TO_ORDER"
+        },
+        "gift": null,
+        "loyalty_card": null,
+        "start_date": null,
+        "expiration_date": "2022-11-10T17:00:00.000Z",
+        "validity_timeframe": null,
+        "validity_day_of_week": null,
+        "active": true,
+        "additional_info": "Diskon 15% Merchant A",
+        "metadata": {
+            "voucher_source": "Merchant"
+        },
+        "assets": {
+            "qr": {
+                "id": "U2FsdGVkX19r3dxZxpN1jzBRb7IrRziGJ1Ok1i9ZZ4fyvKOA/vfe8VpjeHTq6eCT0C+ONPdyReMqh9V2I7JBlnpYne5aqEF02H8U22F2kVfcPjxR6fcOr5aajUNWcK/M",
+                "url": "https://as1.dl.voucherify.io/api/v1/assets/qr/U2FsdGVkX19r3dxZxpN1jzBRb7IrRziGJ1Ok1i9ZZ4fyvKOA%2Fvfe8VpjeHTq6eCT0C%2BONPdyReMqh9V2I7JBlnpYne5aqEF02H8U22F2kVfcPjxR6fcOr5aajUNWcK%2FM"
+            },
+            "barcode": {
+                "id": "U2FsdGVkX1/S/6JJW/v/IAcKZ4EwXnG9y8yPR3+HTGq/q8I0BfAuPz1neB8u62/VRJtAqIUOMYnjzOYIbzc+iSWl3ytKa0bycfcZsW0OH0JOh4EG5gnZyiuU52rdu0Ab",
+                "url": "https://as1.dl.voucherify.io/api/v1/assets/barcode/U2FsdGVkX1%2FS%2F6JJW%2Fv%2FIAcKZ4EwXnG9y8yPR3%2BHTGq%2Fq8I0BfAuPz1neB8u62%2FVRJtAqIUOMYnjzOYIbzc%2BiSWl3ytKa0bycfcZsW0OH0JOh4EG5gnZyiuU52rdu0Ab"
+            }
+        },
+        "is_referral_code": false,
+        "created_at": "2022-10-21T04:13:19.690Z",
+        "updated_at": null,
+        "redemption": {
+            "quantity": null,
+            "redeemed_quantity": 0,
+            "object": "list",
+            "url": "/v1/vouchers/DISC15MA/redemptions?page=1&limit=10"
+        },
+        "publish": {
+            "object": "list",
+            "count": 0,
+            "url": "/v1/vouchers/DISC15MA/publications?page=1&limit=10"
+        },
+        "object": "voucher"
+    },
+    {
+        "id": "v_1GHC8UmOjZ7krdfCXvT5guT5Q12IORNn",
+        "code": "DISC25K",
+        "campaign": null,
+        "campaign_id": null,
+        "category": null,
+        "category_id": null,
+        "type": "DISCOUNT_VOUCHER",
+        "discount": {
+            "type": "PERCENT",
+            "amount_limit": 2500000,
+            "percent_off": 25,
+            "effect": "APPLY_TO_ORDER"
+        },
+        "gift": null,
+        "loyalty_card": null,
+        "start_date": null,
+        "expiration_date": "2022-11-10T17:00:00.000Z",
+        "validity_timeframe": null,
+        "validity_day_of_week": null,
+        "active": true,
+        "additional_info": "Diskon 25K",
+        "metadata": {
+            "voucher_source": "Flik"
+        },
+        "assets": {
+            "qr": {
+                "id": "U2FsdGVkX1/KoxFCsYNEDr/70wf09sAqngCxCCQbc1HlgsYKZN2nrMgx5/LMtApRiPPjinyiE1qsGGv7fsc1eqZpQge5ipPBnnL85wLBoTEIWvWw2t3yabM8oNS5x1I9",
+                "url": "https://as1.dl.voucherify.io/api/v1/assets/qr/U2FsdGVkX1%2FKoxFCsYNEDr%2F70wf09sAqngCxCCQbc1HlgsYKZN2nrMgx5%2FLMtApRiPPjinyiE1qsGGv7fsc1eqZpQge5ipPBnnL85wLBoTEIWvWw2t3yabM8oNS5x1I9"
+            },
+            "barcode": {
+                "id": "U2FsdGVkX19v3dSAMLoX4ecqQV6g4/Cw6VjYoXGyOG2EYEEaWhk8usbNw7gfJI6x8lxaMoPO6m29qrhJMd+5YNx6LTdk8se5D38RgPYqykGLAja1vYv/GqjBIPeo7gry",
+                "url": "https://as1.dl.voucherify.io/api/v1/assets/barcode/U2FsdGVkX19v3dSAMLoX4ecqQV6g4%2FCw6VjYoXGyOG2EYEEaWhk8usbNw7gfJI6x8lxaMoPO6m29qrhJMd%2B5YNx6LTdk8se5D38RgPYqykGLAja1vYv%2FGqjBIPeo7gry"
+            }
+        },
+        "is_referral_code": false,
+        "created_at": "2022-10-21T04:10:11.809Z",
+        "updated_at": null,
+        "redemption": {
+            "quantity": null,
+            "redeemed_quantity": 0,
+            "object": "list",
+            "url": "/v1/vouchers/DISC25K/redemptions?page=1&limit=10"
+        },
+        "publish": {
+            "object": "list",
+            "count": 0,
+            "url": "/v1/vouchers/DISC25K/publications?page=1&limit=10"
+        },
+        "object": "voucher"
+    }
+]
